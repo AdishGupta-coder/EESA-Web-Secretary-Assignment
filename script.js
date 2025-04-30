38fc72281bd5887eab7e4c85921b1f6389c74d78
Adish Gupta <84017733+AdishGupta-coder@users.noreply.github.com>
2025-04-30 23:48:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+    window.mode=true;
+    const toggleButton = document.getElementById('mode');
+    const home = document.getElementById('home');
+    const resources = document.getElementById('resources');
+    toggleButton.textContent="Dark Mode";
+    toggleButton.style.backgroundColor="white";
+    toggleButton.style.color="black";
+
+    toggleButton.addEventListener('click', function() {
+        window.mode = !window.mode;
+        if(window.mode) {
+            toggleButton.textContent = "Dark Mode";
+            toggleButton.backgroundColor="white";
+            toggleButton.style.color="black";
+            home.style.color="black";
+            resources.style.color="black";
+            document.body.classList.remove("dark");
+        }
+        else{
+            toggleButton.textContent="Light Mode";
+            toggleButton.backgroundColor="black";
+            toggleButton.style.color="white";
+            home.style.color="white";
+            resources.style.color="white";
+            document.body.classList.add("dark");
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function (){
+    const slides = document.querySelectorAll('.slide');
+    const l = slides.length;
+    let i = 0;
+
+    document.getElementById('right').addEventListener('click', function(){
+        slides[i].classList.remove("active");
+        i = (i + 1) % l;
+        slides[i].classList.add("active");
+    });
+
+    document.getElementById('left').addEventListener('click', function(){
+        slides[i].classList.remove("active");
+        i = (i - 1 + l) % l;
+        slides[i].classList.add("active");
+    });
+});
